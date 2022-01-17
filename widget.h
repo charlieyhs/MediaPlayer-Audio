388337ce3f9068fd2337c206c1c1de406340ec6d
charlieyhs <87665482+charlieyhs@users.noreply.github.com>
2022-01-17 16:47:57 -0500

    Add files via upload

---
+++ widget.h
@@ -0,0 +1,33 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+
+class QMediaPlayer;
+
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_abrirPushButton_clicked();
+    void on_playPushButton_clicked();
+    void on_pausePushButton_clicked();
+    void on_stopPushButton_clicked();
+    void on_mutePushButton_clicked();
+    void on_volumenHorizontalSlider_valueChanged(int value);
+
+private:
+    Ui::Widget *ui;
+    QMediaPlayer *mMediaPlayer;
+};
+#endif // WIDGET_H
